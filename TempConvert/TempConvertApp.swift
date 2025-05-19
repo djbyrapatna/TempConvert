3ec2e9fe3d8081c839c6d6fd7aa93b329b2c8f7d
Dhruva Byrapatna <dhruvajb@gmail.com>
2025-05-19 16:36:54 -0400

    Initial Commit

---
+++ TempConvert/TempConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempConvertApp.swift
+//  TempConvert
+//
+//  Created by Dhruva Jothik Byrapatna on 5/19/25.
+//
+
+import SwiftUI
+
+@main
+struct TempConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TempConvertTests/TempConvertTests.swift
@@ -0,0 +1,17 @@
+//
+//  TempConvertTests.swift
+//  TempConvertTests
+//
+//  Created by Dhruva Jothik Byrapatna on 5/19/25.
+//
+
+import Testing
+@testable import TempConvert
+
+struct TempConvertTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
